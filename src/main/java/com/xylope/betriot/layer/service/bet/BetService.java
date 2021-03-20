2c1bd3d262d425e896eea62682317472370bdeae
Xylope <XylopeOfficial@gmail.com>
2021-03-21 01:11:06 +0900

    BetService 관련기능 일부 구현

--- src/main/java/com/xylope/betriot/layer/service/bet/BetService.java
+++ src/main/java/com/xylope/betriot/layer/service/bet/BetService.java
@@ -1,7 +1,31 @@
 package com.xylope.betriot.layer.service.bet;
 
+import com.merakianalytics.orianna.Orianna;
+import com.xylope.betriot.layer.dataaccess.riotdata.SummonerDto;
+import com.xylope.betriot.layer.domain.vo.UserVO;
+
+import java.util.List;
+
 public class BetService {
     //3분내로 게임시작
     //틱 이벤트 사용해서 3분 재기
-    //
+    List<BetUser> betUserQueue;
+
+    public BetService() {
+        new Action() {
+            @Override
+            public void doSomething(UserVO user) {
+                Orianna.currentMatchForSummoner(Orianna.summonerWithId(user.getRiotId()).get()).get();
+            }
+        };
+    }
+
+    public void countdown() {
+        betUserQueue.forEach(BetUser::countdown);
+    }
+
+    @FunctionalInterface
+    public interface Action {
+        void doSomething(UserVO user);
+    }
 }
